01b2a23c1281534aa8b323077e04d9e39a6c8a8c
Luisa Escobar-Robledo <lescobar@Lus-MacBook-Air.local>
2025-06-06 15:34:13 -0400

    fixed links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://lescobar-robledo.github.io/',
+    base: '/EllieCompBio',
+});
